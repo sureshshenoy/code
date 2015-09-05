b04219f1642e760182e30ae0158025c66e31a75e
suresh <sureshshenoy@users.noreply.github.com>
2015-09-05 13:08:33 -0700

    Added macros
    added macros to clear, set and toggle bit

--- bitops/bitops.h
+++ bitops/bitops.h
@@ -1,4 +1,10 @@
 #ifndef _BITOPS_H
 #define _BITOPS_H
 
+#define CLEAR_BIT(N, BIT) \ 
+		(N) = (N) & ~(1 << BIT)
+#define SET_BIT(N, BIT) \
+		(N) = (N) | (1 << BIT)
+#define TOGGLE_BIT(N, BIT) \
+		(N) = (N) ^ (1 << BIT)
 #endif
